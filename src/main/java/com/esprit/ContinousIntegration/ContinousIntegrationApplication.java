af5608aa227e8b01a7b2615425fe2076927f9d4e
Chams91 <sunnychamsever@gmail.com>
2019-11-15 19:22:14 +0100

    first commit

---
+++ src/main/java/com/esprit/ContinousIntegration/ContinousIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.ContinousIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinousIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinousIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/ContinousIntegration/ContinousIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.ContinousIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinousIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
